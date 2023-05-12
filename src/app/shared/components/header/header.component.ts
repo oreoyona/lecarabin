d066401507a96bea4e7a7a82a734cf96a5ae5131
Gloire Chabu <cortesgloire@gmail.com>
2023-05-12 14:44:30 +0200

    Created the dialogs for the header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material/dialog';
 import { MenuComponent } from './menu/menu.component';
 import { SearchComponent } from './search/search.component';
 
@@ -8,23 +8,51 @@ import { SearchComponent } from './search/search.component';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnChanges {
 
   logo = "/assets/lecarabin-logo.svg";
+  widthValue = ""; screenHeight!: number; screenWidth!: number;
+
   config: MatDialogConfig = {
     height: "100vh",
-    width: "40vw",
-    position: {top: "0", left: '0'}
+    width: this.widthValue,
+    position: { top: "0", left: '0' }
 
   }
-  openMenu = () =>{
-    this.dialog.open(MenuComponent, this.config)
+  openMenu = () => {
+    this.dialog.open(MenuComponent, { height: "100vh", width: this.widthValue, position: { top: '0', left: '0' } })
   }
 
   openSearch = () => {
-    this.dialog.open(SearchComponent, {height: "100vh", width: "40vw",position: {top: '0', right: '0'}})
+    this.dialog.open(SearchComponent, {
+      height: "100vh", width: this.widthValue,
+      position: { top: '0', right: '0' },
+      panelClass: "actualContainer"
+
+    })
   }
 
 
-  constructor(public dialog: MatDialog){}
+  constructor(public dialog: MatDialog) { }
+  ngOnInit() {
+    this.screenHeight = window.innerHeight
+    this.screenWidth = window.innerWidth
+    window.addEventListener('resize', this.onResize.bind(this));
+    this.screenWidth > 800 ? this.widthValue = "40vw" : this.widthValue = "100vw";
+
+  }
+
+
+  ngOnChanges(): void {
+    this.screenHeight = window.innerHeight
+    this.screenWidth = window.innerWidth
+    this.screenWidth > 800 ? this.widthValue = "40vw" : this.widthValue = "100vw"
+
+  }
+
+  onResize() {
+    this.ngOnChanges();
+  }
+
+
 }
