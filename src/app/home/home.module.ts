4072b3584134c01e94da06995da91b43ee3d14eb
Gloire Chabu <cortesgloire@gmail.com>
2023-05-13 13:49:06 +0200

    Created the components of the Home route

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
+import { MainComponent } from './home/main/main.component';
+import { RightSideComponent } from './home/right-side/right-side.component';
+import { LeftSideComponent } from './home/left-side/left-side.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    MainComponent,
+    RightSideComponent,
+    LeftSideComponent
   ],
   imports: [
     CommonModule,
