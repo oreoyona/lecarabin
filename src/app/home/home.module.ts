891304e1549b3ad3d9dde3b7ed9f7da112b536ee
Gloire Chabu <cortesgloire@gmail.com>
2023-05-17 13:12:03 +0200

    Tested the bckground image property on the article component. Can now proceed with the label etiquette

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
@@ -19,7 +20,9 @@ import { ArticleComponent } from './home/article/article.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
-  ]
+    HomeRoutingModule,
+    HttpClientModule
+  ],
+  providers: [{provide: HttpClient, useClass: HttpClient}]
 })
 export class HomeModule { }
--- src/app/home/home/article/article.component.ts
+++ src/app/home/home/article/article.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./article.component.scss']
 })
 export class ArticleComponent {
-
+  imageUrl = "assets/R.jpeg"
 }
