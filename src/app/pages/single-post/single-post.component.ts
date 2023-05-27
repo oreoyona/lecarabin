f18b6d7dd260b0493b0a72d0659d05c51fb34f98
Gloire Chabu <cortesgloire@gmail.com>
2023-05-27 12:38:16 +0200

    Completed the 1/3 of the work needed to be done on the article element

--- src/app/pages/single-post/single-post.component.ts
+++ src/app/pages/single-post/single-post.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-single-post',
   templateUrl: './single-post.component.html',
   styleUrls: ['./single-post.component.scss']
 })
-export class SinglePostComponent {
+export class SinglePostComponent implements OnInit{
+  date!: string
+  ngOnInit(): void {
+    const laDate = new Date();
+    const maintenant = laDate.toUTCString()
+    this.date = `${laDate.toLocaleDateString()}`
+  }
+
+
+
+
+
 
 }
