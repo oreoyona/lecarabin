4c00d98727f3228ae2210cabbd8c859fdedf1b52
Gloire Chabu <cortesgloire@gmail.com>
2023-06-05 13:50:10 +0200

    Defined a new dir for images

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,15 @@
 import os
 
+# TODO: Should be env variables
+UPLOAD_IMAGE_PATH: str = "./lc_contents/images"
+
 class Config(object):
     SECRET_KEY = os.urandom(32)
     basedir = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = 'postgresql://lecarabinuser:0000@localhost/lecarabin'
     SQLAlCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_IMAGE_PATH = UPLOAD_IMAGE_PATH
+
 
 class devConfig(Config):
     DEBUG = True
--- backend/lecarabin.py
+++ backend/lecarabin.py
@@ -1,43 +1,49 @@
 """The entry point of lecarabin"""
 import mimetypes
-from flask import Flask, render_template
-from flask_admin.contrib.sqla import ModelView
-from flask_sqlalchemy import SQLAlchemy
+
+import requests
+from flask import Flask, render_template, redirect
 from flask_migrate import Migrate
-from flask_admin import Admin
 from models import db, User, Post, Image
-from forms import *
 from flask_ckeditor import CKEditor
-from views import ImageView
+import os
+
 app = Flask(__name__, static_url_path='', static_folder='templates/', template_folder='templates')
 app.config.from_object('config.devConfig')
 ckeditor = CKEditor(app)
 
 db.init_app(app)
 migrate = Migrate(app=app, db=db)
-# admin = Admin(app)
-# admin.add_view(ModelView(User, db.session))
-# admin.add_view(ModelView(Post, db.session))
-# admin.add_view(ImageView(Image, db.session))
-# the code below allows the js in our index.html file to be read properly####
 #############################################################################
 mimetypes.add_type('application/javascript', '.js')
 mimetypes.add_type('text/css', '.css')
 
-############################################################################
 
+############################################################################
 
 #############################################################################
 # Routes config
 #############################################################################
 @app.route("/")
 def go_to_home():
-    return render_template('index.html')
+  return render_template('index.html')
+
 
 @app.route("/dashboard")
 def go_to_admin():
-    return render_template('admin.html', title="Dashboard")
+  return render_template('admin.html', title="Dashboard")
+
+
+@app.route("/npost", methods=["GET", "POST"])
+def image_upload():
+  if requests.method == 'POST':
+    file = requests.files['files']
+    if file is None:
+      redirect(requests.url)
+
+    if file.filename == '':
+      pass
 
 
 if __name__ == "__main__":
-    app.run()
+  app.run(debug=True)
